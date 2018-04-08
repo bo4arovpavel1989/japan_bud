39674cea2d7f3558b34c9fde6636452da978be32
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2018-04-08 15:56:36 +0300

    added EMS

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -73,30 +73,66 @@ function calculateTotalWeight(){
 	var fedexPrice = calculateFedexPrice(totalWeight);
 	$('#postprice').html(fedexPrice[0]);
 	$('#posttrackprice').html(fedexPrice[1]);
+	$('#emsprice').html(fedexPrice[2])
 	$('#fedexPrice').val(fedexPrice[0])
 }
 
 function calculateFedexPrice(totalWeight){
 	if (totalWeight <= 100) return [130,360]
-	else if(totalWeight > 100 && totalWeight <= 200) return [180,420]
-	else if(totalWeight > 200 && totalWeight <= 300) return [230,480]
-	else if(totalWeight > 300 && totalWeight <= 400) return [290,540]
-	else if(totalWeight > 400 && totalWeight <= 500) return [350,600]
-	else if(totalWeight > 500 && totalWeight <= 600) return [410,660]
-	else if(totalWeight > 600 && totalWeight <= 700) return [470,720]
-	else if(totalWeight > 700 && totalWeight <= 800) return [530,780]
-	else if(totalWeight > 800 && totalWeight <= 900) return [590,840]
-	else if(totalWeight > 900 && totalWeight <= 1000) return [650,900]
-	else if(totalWeight > 1000 && totalWeight <= 1100) return [710,960]
-	else if(totalWeight > 1100 && totalWeight <= 1200) return [770,1020]
-	else if(totalWeight > 1200 && totalWeight <= 1300) return [830,1080]
-	else if(totalWeight > 1300 && totalWeight <= 1400) return [890,1140]
-	else if(totalWeight > 1400 && totalWeight <= 1500) return [950,1200]
-	else if(totalWeight > 1500 && totalWeight <= 1600) return [1010,1260]
-	else if(totalWeight > 1600 && totalWeight <= 1700) return [1070,1320]
-	else if(totalWeight > 1700 && totalWeight <= 1800) return [1130,1380]
-	else if(totalWeight > 1800 && totalWeight <= 1900) return [1190,1440]
-	else if(totalWeight > 1900) return [1250,1500];
+	else if(totalWeight > 100 && totalWeight <= 200) return [180,420,1200]
+	else if(totalWeight > 200 && totalWeight <= 300) return [230,480,1200]
+	else if(totalWeight > 300 && totalWeight <= 400) return [290,540,1200]
+	else if(totalWeight > 400 && totalWeight <= 500) return [350,600,1200]
+	else if(totalWeight > 500 && totalWeight <= 600) return [410,660,1400]
+	else if(totalWeight > 600 && totalWeight <= 700) return [470,720,1500]
+	else if(totalWeight > 700 && totalWeight <= 800) return [530,780,1600]
+	else if(totalWeight > 800 && totalWeight <= 900) return [590,840,1700]
+	else if(totalWeight > 900 && totalWeight <= 1000) return [650,900,1800]
+	else if(totalWeight > 1000 && totalWeight <= 1100) return [710,960,2100]
+	else if(totalWeight > 1100 && totalWeight <= 1200) return [770,1020,2100]
+	else if(totalWeight > 1200 && totalWeight <= 1250) return [830,1080,2100]
+	else if(totalWeight > 1250 && totalWeight <= 1300) return [830,1080,2300]
+	else if(totalWeight > 1300 && totalWeight <= 1400) return [890,1140,2300]
+	else if(totalWeight > 1400 && totalWeight <= 1500) return [950,1200,2300]
+	else if(totalWeight > 1500 && totalWeight <= 1600) return [1010,1260,2600]
+	else if(totalWeight > 1600 && totalWeight <= 1700) return [1070,1320,2600]
+	else if(totalWeight > 1700 && totalWeight <= 1750) return [1130,1380,2600]
+	else if(totalWeight > 1750 && totalWeight <= 1800) return [1130,1380,2900]
+	else if(totalWeight > 1800 && totalWeight <= 1900) return [1190,1440,2900]
+	else if(totalWeight > 1900 && totalWeight <= 2000) return [1250,1500,2900]
+	else if(totalWeight > 2000 && totalWeight <= 2500) return [1250,1500,3300]
+	else if(totalWeight > 2500 && totalWeight <= 3000) return [1250,1500,3800]
+	else if(totalWeight > 3000 && totalWeight <= 3500) return [1250,1500,4200]
+	else if(totalWeight > 3500 && totalWeight <= 4000) return [1250,1500,4700]
+	else if(totalWeight > 4000 && totalWeight <= 4500) return [1250,1500,5200]
+	else if(totalWeight > 4500 && totalWeight <= 5000) return [1250,1500,5600]
+	else if(totalWeight > 5000 && totalWeight <= 5500) return [1250,1500,6100]
+	else if(totalWeight > 5500 && totalWeight <= 6000) return [1250,1500,6600]
+	else if(totalWeight > 6000 && totalWeight <= 7000) return [1250,1500,7300]
+	else if(totalWeight > 7000 && totalWeight <= 8000) return [1250,1500,8100]
+	else if(totalWeight > 8000 && totalWeight <= 9000) return [1250,1500,8800]
+	else if(totalWeight > 9000 && totalWeight <= 10000) return [1250,1500,9600]
+	else if(totalWeight > 10000 && totalWeight <= 11000) return [1250,1500,10300]
+	else if(totalWeight > 11000 && totalWeight <= 12000) return [1250,1500,11100]
+	else if(totalWeight > 12000 && totalWeight <= 13000) return [1250,1500,11900]
+	else if(totalWeight > 13000 && totalWeight <= 14000) return [1250,1500,12600]
+	else if(totalWeight > 14000 && totalWeight <= 15000) return [1250,1500,13300]
+	else if(totalWeight > 15000 && totalWeight <= 16000) return [1250,1500,14100]
+	else if(totalWeight > 16000 && totalWeight <= 17000) return [1250,1500,14900]
+	else if(totalWeight > 17000 && totalWeight <= 18000) return [1250,1500,15600]
+	else if(totalWeight > 18000 && totalWeight <= 19000) return [1250,1500,16400]
+	else if(totalWeight > 19000 && totalWeight <= 20000) return [1250,1500,17100]
+	else if(totalWeight > 20000 && totalWeight <= 21000) return [1250,1500,17900]
+	else if(totalWeight > 21000 && totalWeight <= 22000) return [1250,1500,18600]
+	else if(totalWeight > 22000 && totalWeight <= 23000) return [1250,1500,19400]
+	else if(totalWeight > 23000 && totalWeight <= 24000) return [1250,1500,20100]
+	else if(totalWeight > 24000 && totalWeight <= 25000) return [1250,1500,20900]
+	else if(totalWeight > 25000 && totalWeight <= 26000) return [1250,1500,21600]
+	else if(totalWeight > 26000 && totalWeight <= 27000) return [1250,1500,22400]
+	else if(totalWeight > 27000 && totalWeight <= 28000) return [1250,1500,23200]
+	else if(totalWeight > 28000 && totalWeight <= 29000) return [1250,1500,23900]
+	else if(totalWeight > 29000 && totalWeight <= 30000) return [1250,1500,24700]
+	else if(totalWeight > 30000) return [1250,1500,24700]
 }
 
 function selectPunkt(punktInfo) { 
@@ -135,16 +171,16 @@ function cartSubmit() {
 		data.comment = $('#customerComment').val();
 		data.fedexPrice = $('#fedexPrice').val();
 		$.ajax({
-        type: "POST",
-        url: "/makeorder",
-        data: data,
-        contentType: "application/x-www-form-urlencoded",
-        success: function(){
-			alert("Заказ принят в работу!");
-			deleteCookie('cart');
-			location.assign('/');
-		}
-  });
+			type: "POST",
+			url: "/makeorder",
+			data: data,
+			contentType: "application/x-www-form-urlencoded",
+			success: function(){
+				alert("Заказ принят в работу!");
+				deleteCookie('cart');
+				location.assign('/');
+			}
+		});
 	});
 }
 
@@ -166,6 +202,8 @@ function setFedexPrice(id){
 		price = Number($('#postprice').html());
 	else if (id.attr('id') ==='track')	
 		price = Number($('#posttrackprice').html());
+	else if (id.attr('id') ==='ems')	
+		price = Number($('#emsprice').html());
 	
 	$('#fedexPrice').val(price)
 }
